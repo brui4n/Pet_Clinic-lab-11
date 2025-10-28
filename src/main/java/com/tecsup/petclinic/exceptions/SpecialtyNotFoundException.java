a06f0d45f35924aeb3ba29287ba24add99b9ce73
Bryan <bryan@MacBook-Air-BC.local>
2025-10-27 21:35:59 -0500

    Desarrollar SpecialtyNotFoundException

--- src/main/java/com/tecsup/petclinic/exceptions/SpecialtyNotFoundException.java
+++ src/main/java/com/tecsup/petclinic/exceptions/SpecialtyNotFoundException.java
@@ -1,5 +1,10 @@
 package com.tecsup.petclinic.exceptions;
 
-public class SpecialtyNotFoundException {
-    
+public class SpecialtyNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public SpecialtyNotFoundException(String message) {
+        super(message);
+    }
 }
