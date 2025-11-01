ffb2231a48d8e67564641c254900cd4bf39f0843
RonaldoMeza <ronaldo.meza@tecsup.edu.pe>
2025-11-01 11:07:30 -0500

    Implementación de SpecialtyService

--- src/main/java/com/tecsup/petclinic/services/SpecialtyService.java
+++ src/main/java/com/tecsup/petclinic/services/SpecialtyService.java
@@ -1,5 +1,23 @@
 package com.tecsup.petclinic.services;
 
-public class SpecialtyService {
-    
+import java.util.List;
+
+import com.tecsup.petclinic.entities.Specialty;
+import com.tecsup.petclinic.exceptions.SpecialtyNotFoundException;
+
+public interface SpecialtyService {
+
+    Specialty create(Specialty speciality);
+
+    Specialty update(Specialty speciality);
+
+    void delete(Integer id) throws SpecialtyNotFoundException;
+
+    Specialty findById(Integer id) throws SpecialtyNotFoundException;
+
+    List<Specialty> findByName(String name);
+
+    List<Specialty> findByDescriptionContaining(String keyword);
+
+    List<Specialty> findAll();
 }
